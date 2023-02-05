df73fc7bcabd939dc098d2d22744af94b04daa23
phonon-py <phn.19890918@gmail.com>
2023-02-06 06:46:05 +0900

    1st commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,21 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Likebutton />
       </header>
     </div>
   );
+}
+
+function Likebutton(){
+  const [count, setCont] = useState(999);
+  return <span className='likeButton'>♥ {count}</span>;
 }
 
 export default App;
